1652a43649828acf698c4a8c459703a7479c62a0
JinJuJeong <wjdwlswn731@gmail.com>
2022-12-03 19:33:10 +0900

    :bookmark: My Minimal Account Book 1.0

--- web/resources/sql/expenditure.sql
+++ web/resources/sql/expenditure.sql
@@ -8,5 +8,5 @@ create table if not exists Expenditure
     category    VARCHAR(50)   NOT NULL,
     description VARCHAR(3000) NOT NULL,
     tag         VARCHAR(3000) NOT NULL,
-    PRIMARY KEY (expend_id)
+    PRIMARY KEY (expend_id, email_id)
 )default CHARSET=utf8;
